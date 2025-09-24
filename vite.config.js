a2d595518e4f1cf164cf66608f9958f32932fa7c
Ingetes <comercial@ingetes.com>
2025-09-24 10:35:32 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Si vas a usar dominio propio o Vercel, puedes dejar base: './'.
 export default defineConfig({
   plugins: [react()],
-  base: '/Medicion-KPI/',
+  base: '/Medicion-KPI/',   // <-- usa EXACTAMENTE el nombre de tu repo
 })
